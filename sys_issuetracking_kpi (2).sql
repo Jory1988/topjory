7c7bb91ce5cd5e74d32736767a61b3a895c99912
Jory1988 <68308837+Jory1988@users.noreply.github.com>
2020-07-15 09:39:03 +0800

    Add files via upload

---
+++ sys_issuetracking_kpi (2).sql
@@ -0,0 +1,3 @@
+INSERT INTO `sys_issuetracking_kpi` VALUES ('5', '211', 'UPPH-Process-L6', 'gwh', 1, '2020-07-14 16:07:35', NULL);
+INSERT INTO `sys_issuetracking_kpi` VALUES ('6', '211', 'FPY-Process-L6', 'gwh', 1, '2020-07-14 16:07:35', NULL);
+INSERT INTO `sys_issuetracking_kpi` VALUES ('7', '211', 'HC-Process-L6', 'gwh', 1, '2020-07-14 16:07:35', NULL);
